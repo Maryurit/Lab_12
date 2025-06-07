eaa345efc3a05f16678c2a407f68c4e6f2d19183
Maryurit <maryorit@gmail.com>
2025-06-06 20:42:46 -0500

    first commit

---
+++ src/main/java/com/tecsup/semana_12_apli01/Semana12Apli01Application.java
@@ -0,0 +1,11 @@
+package com.tecsup.semana_12_apli01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Semana12Apli01Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Semana12Apli01Application.class, args);
+    }
+}
